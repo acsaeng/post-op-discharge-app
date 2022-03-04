e4f227ba343bfa22b1071a501abbf6ddca0c756a
theCivilCoder <yongjunjz@hotmail.com>
2022-03-03 21:24:41 -0700

    removed margin bottom mb, made the fixed to the top with a hidden layer underneath to push subsequent divs down

--- frontend/src/navbar/Navbar.js
+++ frontend/src/navbar/Navbar.js
@@ -3,16 +3,21 @@ import { FaUserCircle } from 'react-icons/fa'
 
 const Navbar = () => {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light justify-content-between mb-5" style={{"background-color": "#e9ecef"}}>
-        <a class="navbar-brand mx-3 fs-3" style={{"color": "#162a53"}} href="/"><FaUserCircle /></a>
-
-        <form class="d-flex form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2 me-2" type="search" placeholder="Search"></input>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <button class="btn btn-outline-light my-2 me-2 my-sm-0" style={{"background-color": "#162a53"}} type="submit">Search</button>
-            </div>
-        </form>
+    <div>
+    <nav className="navbar navbar-expand-lg navbar-light justify-content-between w-100" style={{"backgroundColor": "#e9ecef","top":0, "height":"75px"}}>
     </nav>
+    <nav className="navbar navbar-expand-lg navbar-light justify-content-between w-100" style={{"backgroundColor": "#e9ecef", "position":"fixed", "top":0, "height":"75px"}}>
+            <a className="navbar-brand mx-3 fs-3" style={{"color": "#162a53"}} href="/"><FaUserCircle /></a>
+
+            <form className="d-flex form-inline my-2 my-lg-0">
+                <input className="form-control mr-sm-2 me-2" type="search" placeholder="Search"></input>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <button className="btn btn-outline-light my-2 me-2 my-sm-0" style={{"backgroundColor": "#162a53"}} type="submit">Search</button>
+                </div>
+            </form>
+        </nav>
+    </div>
+   
   )
 }
 
